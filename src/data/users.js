5b598fb6609b263ffcdd660548ca1fae643a0b97
adnansdeofficial@gmail.com <adnansdeofficial@gmail.com>
2023-06-12 00:07:31 +0530

    added the users data

---
+++ src/data/users.js
@@ -0,0 +1,19 @@
+const userCredentialsArray = [
+    {
+      username: "user1",
+      password: "password1",
+      jetToken: "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4NjUwODUwOSwiaWF0IjoxNjg2NTA4NTA5fQ.e1yRdG1rHJD6-eYFrzKlYXC1vi6oVF0lTu_cyz7UB2k"
+    },
+    {
+      username: "user2",
+      password: "password2",
+      jetToken: "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNjg2NTA4NTA5LCJpYXQiOjE2ODY1MDg1MDl9.x7WEmQc6I2n7vToABOwAuMS3tFIJ-AUWp-qqLNXd7UA"
+    },
+    {
+      username: "user3",
+      password: "password3",
+      jetToken: "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoicGFzc3dvcmQgMyIsIklzc3VlciI6Iklzc3VlciIsIlVzZXJuYW1lIjoidXNlcjMiLCJleHAiOjE2ODY1MDg1MDksImlhdCI6MTY4NjUwODUwOX0.sdDQVec3ZVxR61XPYh8m_g-5W4-PzzRf3BEmKOO0jgw"
+    }
+  ];
+
+  export default  userCredentialsArray
