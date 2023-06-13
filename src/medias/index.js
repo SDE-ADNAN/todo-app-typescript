087016ec4541442e5567e8757ce3893ab4c1cd06
adnansdeofficial@gmail.com <adnansdeofficial@gmail.com>
2023-06-13 14:36:20 +0530

    images added

--- src/medias/index.js
+++ src/medias/index.js
@@ -3,6 +3,8 @@ import DeleteImg from "./remove.png"
 import hide from "./hidden.png"
 import show from "./view.png"
 import rightArrow from "./rightarrow.svg"
+import edit from "./icons8-edit-512.svg"
+import childTodo from "./subtask.svg"
 
 
-export { AddImg , DeleteImg , hide ,show, rightArrow}
+export { AddImg , DeleteImg , hide ,show, rightArrow,edit,childTodo}
