37cce20cec21167a29664cb7b44b178a13c50866
adnansdeofficial@gmail.com <adnansdeofficial@gmail.com>
2023-06-13 14:36:05 +0530

    changes

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useContext, ChangeEvent, useEffect} from "react";
-import { TodoItem} from "../App";
+import React, { useState, useContext, ChangeEvent, useEffect } from "react";
+import { TodoItem } from "../App";
 import { TodoContext } from "../context/todoContext";
 import "./Todo.scss";
-import { AddImg, DeleteImg, rightArrow } from "../medias";
+import { AddImg, DeleteImg, edit, rightArrow } from "../medias";
+import NoofSubtodos from "./UIComponents/NoofSubtodos";
 
 interface TodoProps {
   todo: TodoItem;
@@ -10,9 +11,19 @@ interface TodoProps {
 }
 
 const Todo: React.FC<TodoProps> = ({ todo, todoKey }) => {
-  const { todos, addTodo, deleteTodo, setShowAddInput , setIsCompleted , setShowSubTodos} = useContext(TodoContext);
+  const {
+    todos,
+    addTodo,
+    deleteTodo,
+    setShowAddInput,
+    setIsCompleted,
+    setShowSubTodos,
+    setNewTitle,
+  } = useContext(TodoContext);
   const [subTodoText, setSubTodoText] = useState("");
-  const [checkboxDisabled ,setCheckboxDisabled] = useState(false)
+  const [todoTitle, setTodoTitle] = useState(todo.title);
+  const [showTodoTitleInput, setShowTodoTitleInput] = useState(false);
+  const [checkboxDisabled, setCheckboxDisabled] = useState(false);
 
   const handleSubTodoTextChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -20,12 +31,11 @@ const Todo: React.FC<TodoProps> = ({ todo, todoKey }) => {
     setSubTodoText(event.target.value);
   };
 
-
   const handleAddTodo = (parentId: string, title: string) => {
     // Call the addTodo function with the required parameters
     addTodo(parentId, title);
     setSubTodoText("");
-    setShowAddInput(todoKey, false)
+    setShowAddInput(todoKey, false);
   };
 
   const handleDeleteTodo = (id: string) => {
@@ -35,54 +45,65 @@ const Todo: React.FC<TodoProps> = ({ todo, todoKey }) => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    if(subTodoText.length === 0){
-      alert("do a valid input todos cant be empty")
-    }else
-    handleAddTodo(todoKey, subTodoText)
-  }
-
-  const RenderConditionalTodos = () => {
-    console.log(todo.showSubtodos)
-    if (todo.todo != null) {
-      return (<ul className={`adc ${!todo.showSubtodos && "hide"}`}>
-        {todo.todo.map((subTodo) => (
-          <Todo
-            todo={subTodo}
-            todoKey={subTodo.id}
-          />
-        ))}
-      </ul>)
-    } else
-      if (todo.todo === null && todos.length > 0) {
-        return (<ul className={`xyz  ${!todo.showSubtodos && "hide"}`}>
-          {todos.map((subTodo) => (
-            <Todo
-              todo={subTodo}
-              todoKey={subTodo.id}
-            />
-          ))}
-        </ul>)
-      }
-    return (
-      <div>nope</div>
-    )
-  }
-
-  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>,todoId:string) => {
-    const { checked } = event.target;
-    setIsCompleted(todoId,checked)
+    if (subTodoText.length === 0) {
+      alert("do a valid input todos cant be empty");
+    } else handleAddTodo(todoKey, subTodoText);
+  };
+  const handleTodoTitleSubmit = (e: any) => {
+    e.preventDefault();
+    if (todoTitle.length === 0) {
+      alert("do a valid input todos cant be empty");
+    } else {
+      setNewTitle(todoKey, todoTitle);
+    }
   };
 
-  useEffect(()=>{
-    const allCompleted = todo.todo.map((todo)=>todo.isCompleted === true).some(value => value === false)
-    if(todo.todo.length > 0 && !allCompleted){
-      setCheckboxDisabled(false)
-    }else if(todo.todo.length > 0 && allCompleted){
-      setCheckboxDisabled(true)
-    }else if(todo.todo.length === 0){
-      setCheckboxDisabled(false)
+  const RenderConditionalTodos = () => {
+    console.log(todo.showSubtodos);
+    if (todo.todo != null) {
+      return (
+        <ul className={`adc ${!todo.showSubtodos && "hide"}`}>
+          {todo.todo.map((subTodo) => (
+            <Todo todo={subTodo} todoKey={subTodo.id} />
+          ))}
+        </ul>
+      );
+    } else if (todo.todo === null && todos.length > 0) {
+      return (
+        <ul className={`xyz  ${!todo.showSubtodos && "hide"}`}>
+          {todos.map((subTodo) => (
+            <Todo todo={subTodo} todoKey={subTodo.id} />
+          ))}
+        </ul>
+      );
     }
-  },[todo])
+    return <div>nope</div>;
+  };
+
+  const handleCheckboxChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    todoId: string
+  ) => {
+    const { checked } = event.target;
+    setIsCompleted(todoId, checked);
+  };
+  const handleTodoTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newTitle = event.target.value;
+    setTodoTitle(newTitle);
+  };
+
+  useEffect(() => {
+    const allCompleted = todo.todo
+      .map((todo) => todo.isCompleted === true)
+      .some((value) => value === false);
+    if (todo.todo.length > 0 && !allCompleted) {
+      setCheckboxDisabled(false);
+    } else if (todo.todo.length > 0 && allCompleted) {
+      setCheckboxDisabled(true);
+    } else if (todo.todo.length === 0) {
+      setCheckboxDisabled(false);
+    }
+  }, [todo]);
 
   return (
     <div
@@ -90,34 +111,82 @@ const Todo: React.FC<TodoProps> = ({ todo, todoKey }) => {
       className={`todo_container ${todo.isCompleted && "completed"}  `}
     >
       <div className="subTodos_container">
-      <div className="head_container">
-        <div className="CTAS_container">
-          
-          {todo.title}
-          <div className="CTAS"><div title={"Delete this todo "} onClick={() => handleDeleteTodo(todoKey)}>
-            <img src={DeleteImg} alt={"images"}></img>
+        <div className="head_container">
+          <div className="CTAS_container">
+            {todo.todo.length > 0 && (
+              <img
+                onClick={() => setShowSubTodos(todo.id)}
+                className={`rightarrow  ${todo.showSubtodos && "rotate-90deg"}`}
+                src={rightArrow}
+                alt={"right-arrow"}
+              ></img>
+            )}
+            {!showTodoTitleInput ? (
+              <div className="todo_title truncate-text">{todo.title}</div>
+            ) : (
+              <div className="todo_title truncate-text">
+                <form onSubmit={handleTodoTitleSubmit}>
+                  <input
+                    type="textarea"
+                    value={todoTitle}
+                    onChange={handleTodoTitleChange}
+                  ></input>
+                </form>
+              </div>
+            )}
+            <div className="CTAS">
+              {todo.todo.length > 0 &&
+                <NoofSubtodos subTodoNumber={todo.todo.length}/>
+              }
+              <div
+                title={"Delete this todo "}
+                onClick={() => handleDeleteTodo(todoKey)}
+              >
+                <img src={DeleteImg} alt={"images"}></img>
+              </div>
+              <div
+                className={`add_btn`}
+                title={"Add a subtodo"}
+                onClick={() => setShowAddInput(todoKey, true)}
+              >
+                <img src={AddImg} alt={"images"}></img>
+              </div>
+              <div
+                className={`edit_btn`}
+                title={"Edit"}
+                onClick={() => setShowTodoTitleInput(!showTodoTitleInput)}
+              >
+                <img src={edit} alt={"edit"}></img>
+              </div>
+              
+            </div>
           </div>
-          <div className={`add_btn`} title={"Add a subtodo"} onClick={() => setShowAddInput(todoKey, true)}>
-            <img src={AddImg} alt={"images"}></img>
-          </div></div>
-          {todo.todo.length >0 && <img onClick={()=>setShowSubTodos(todo.id)} className={`rightarrow  ${todo.showSubtodos && "rotate-90deg"}`} src={rightArrow} alt={"right-arrow"}></img>}
-          
+          <div className={`btns `}>
+            {todo.showInput && (
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={subTodoText}
+                  onChange={handleSubTodoTextChange}
+                />
+              </form>
+            )}
+          </div>
+
+          <RenderConditionalTodos />
         </div>
-        <div className={`btns `}>
-          {todo.showInput && <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={subTodoText}
-              onChange={handleSubTodoTextChange}
-            />
-          </form>}
+        <div className="checkbox">
+          <input
+            type="checkbox"
+            disabled={checkboxDisabled}
+            checked={todo.isCompleted}
+            title={`${
+              checkboxDisabled ? "first complete subtodos" : "Complete"
+            } `}
+            onChange={(e) => handleCheckboxChange(e, todo.id)}
+          ></input>
         </div>
-        
-        <RenderConditionalTodos />
       </div>
-      <div className="checkbox"><input type="checkbox" disabled={checkboxDisabled} checked={todo.isCompleted} title={`${checkboxDisabled ? "first complete subtodos" : "Complete"} `} onChange={(e)=>handleCheckboxChange(e,todo.id)}></input></div>
-      </div>
-      
     </div>
   );
 };
