9ad48acae6a6ef79169305469b1e75fdde93045b
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-10 09:12:14 +0530

    some needy code

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,8 @@ import { TodoContextProvider } from './context/todoContext';
 
 ReactDOM.render(<TodoContextProvider><App /></TodoContextProvider>, document.getElementById('root'));
 
+// Before
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// After
+// createRoot(document.getElementById('root')).render(<App />);
