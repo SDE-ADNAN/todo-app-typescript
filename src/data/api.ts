793d56f3e0aea5aeb51d3bc6780945a9637b03c8
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-21 18:59:12 +0530

    fix

---
+++ src/data/api.ts
@@ -0,0 +1,9 @@
+
+// local one
+export const API_URL_LOCAL = "http://localhost:3033"
+
+// live one
+export const API_URL_LIVE = "https://adnans-todo-backend.onrender.com"
+
+// isLive
+export const isLive = true;
