47ddd121c3ad2d824c9a2e49b88335647d195968
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-21 19:12:43 +0530

    added Modal

---
+++ src/components/UIComponents/Modal/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC, ReactNode } from 'react';
+import './Modal.scss';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
