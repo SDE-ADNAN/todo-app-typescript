7a7eb21a013df0809b43274a7cd50c08c36aaf82
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-27 21:32:40 +0530

    register Page added

---
+++ src/Pages/Admin/RegisterPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Register from '../../components/Admin/Register/Register'
+
+interface RegisterPageProps {
+
+}
+
+const RegisterPage: React.FC<RegisterPageProps> = () => {
+    return (
+        <Register />
+    )
+}
+
+export default RegisterPage
