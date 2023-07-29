18b3becf399d370bfc185bf12a52747126867caf
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-29 15:49:30 +0530

    isauth check

--- src/Pages/Admin/RegisterPage.tsx
+++ src/Pages/Admin/RegisterPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Register from '../../components/Admin/Register/Register'
+import { useNavigate } from 'react-router-dom';
 
 interface RegisterPageProps {
-
+    setIsAuthenticated: any;
 }
 
-const RegisterPage: React.FC<RegisterPageProps> = () => {
+const RegisterPage: React.FC<RegisterPageProps> = ({ setIsAuthenticated }) => {
+    const navigate = useNavigate()
+    useEffect(() => {
+        const token = localStorage.getItem('Token');
+        if (!token) {
+            setIsAuthenticated(false)
+        } else {
+            setIsAuthenticated(true)
+            navigate('/dashboard')
+        }
+    }, []);
     return (
         <Register />
     )
