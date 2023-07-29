a7f1281c70801735fda8189fecfe31b1a58707b0
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-29 15:49:20 +0530

    isauthenticated check

--- src/Pages/Admin/LoginPage.tsx
+++ src/Pages/Admin/LoginPage.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
 import Login from "../../components/Admin/Login/Login"
+import { useNavigate } from "react-router-dom";
 
 export interface LoginPageProps {
+    setIsAuthenticated: any;
 }
-const LoginPage: React.FC<LoginPageProps> = () => {
+const LoginPage: React.FC<LoginPageProps> = ({ setIsAuthenticated }) => {
+    const navigate = useNavigate()
+    useEffect(() => {
+        const token = localStorage.getItem('Token');
+        if (!token) {
+            setIsAuthenticated(false)
+        } else {
+            setIsAuthenticated(true)
+            navigate('/dashboard')
+        }
+    }, []);
     return(
-        <Login />
+        <Login setIsAuthenticated={setIsAuthenticated} />
     )
 }
 
