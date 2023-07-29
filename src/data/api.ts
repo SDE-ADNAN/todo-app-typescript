a1fc819acffeaf101e279334466437243bda330d
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-29 15:48:47 +0530

    some changes

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,9 +1,23 @@
 
 // local one
-export const API_URL_LOCAL = "http://localhost:3033"
+export const API_URL_LOCAL = "http://192.168.0.101:3033"
 
 // live one
 export const API_URL_LIVE = "https://adnans-todo-backend.onrender.com"
 
 // isLive
-export const isLive = false;
+export const isLive = true;
+
+
+export function generateUniqueID() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const length = 32;
+    let id = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      id += characters.charAt(randomIndex);
+    }
+  
+    return id;
+}
