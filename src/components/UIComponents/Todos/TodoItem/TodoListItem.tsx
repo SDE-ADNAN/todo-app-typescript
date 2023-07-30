4515139cb9e8e929a73cc3cdb5d6adf43336bf2e
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-30 22:08:55 +0530

    TodoListItem added

--- src/components/UIComponents/Todos/TodoItem/TodoListItem.tsx
+++ src/components/UIComponents/Todos/TodoItem/TodoListItem.tsx
@@ -3,13 +3,25 @@ import './TodoItem.scss';
 import ChevronIcon from '../../Chevron/ChevronIcon';
 
 interface TodoListItemProps {
+    item: {
+        createdAt: string;
     title: string;
+        todo: any[];
+        updatedAt: string;
+        user: string;
+        __v: number;
+        _id: string;
+    }
+
 }
 
-const TodoListItem: React.FC<TodoListItemProps> = ({ title }) => {
+const TodoListItem: React.FC<Partial<TodoListItemProps>> = ({ item }) => {
+    if (!item) {
+        return null
+    }
     return (
-        <div className='todo_item_individual'>
-            <div className='todo_item_title truncate'>{title}</div>
+        <div id={item._id} className='todo_item_individual'>
+            <div className='todo_item_title truncate'>{item && item.title}</div>
             <div className="todo_CTAs_container">
                 <ChevronIcon tooltipText={'Details Page'} />
             </div>
