ea852aba333bc16c67513a4059a223b3c193fd7c
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-31 23:50:46 +0530

    redux token used

--- src/Pages/Admin/LoginPage.tsx
+++ src/Pages/Admin/LoginPage.tsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import Login from "../../components/Admin/Login/Login"
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../ReduxStore/store";
 
 export interface LoginPageProps {
     setIsAuthenticated: any;
 }
 const LoginPage: React.FC<LoginPageProps> = ({ setIsAuthenticated }) => {
     const navigate = useNavigate()
-    useEffect(() => {
-        const token = localStorage.getItem('Token');
-        if (!token) {
-            setIsAuthenticated(false)
-        } else {
-            setIsAuthenticated(true)
-            navigate('/dashboard')
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const reduxToken = useSelector((state: RootState) => state.User.token)
+    // useEffect(() => {
+    //     const token = localStorage.getItem('Token');
+    //     if (!token && !reduxToken) {
+    //         setIsAuthenticated(false)
+    //     } else {
+    //         setIsAuthenticated(true)
+    //         navigate('/dashboard')
+    //     }
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
     return(
         <Login setIsAuthenticated={setIsAuthenticated} />
     )
