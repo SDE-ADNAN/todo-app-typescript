63d42b2ae0aefd40a82c09ccc1974e7e0075b554
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-31 23:50:33 +0530

    formatDateAndTime func added

--- src/CONFIG/index.ts
+++ src/CONFIG/index.ts
@@ -2,8 +2,30 @@ import { API_URL_LIVE, API_URL_LOCAL, isLive } from "../api";
 
 export const getUrl =(remUrl:string) => `${isLive ? API_URL_LIVE : API_URL_LOCAL}${remUrl}`
 
+export function formatDateAndTime(dateObj: Date): [string, string] {
+    const optionsDate: Intl.DateTimeFormatOptions = {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    };
+  
+    const optionsTime: Intl.DateTimeFormatOptions = {
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+  
+    const dateFormatter = new Intl.DateTimeFormat('en-US', optionsDate);
+    const timeFormatter = new Intl.DateTimeFormat('en-US', optionsTime);
+  
+    const formattedDate = dateFormatter.format(dateObj);
+    const formattedTime = timeFormatter.format(dateObj);
+  
+    return [formattedDate, formattedTime];
+  }
+
 const Todos = {
-    getUrl:getUrl
+    getUrl:getUrl,
+    formatDateAndTime:formatDateAndTime
 }
 
 export default Todos;
