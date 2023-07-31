a01c4e176ac9d73810d664af1b110e17b0fd8882
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-31 23:46:15 +0530

    added Browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './ReduxStore/store';
+import { BrowserRouter } from "react-router-dom";
 
 const ReduxProvider = Provider
 
-ReactDOM.render(<ReduxProvider store={store}><App /></ReduxProvider>, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <ReduxProvider store={store}>
+                <App />
+            </ReduxProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+    , document.getElementById('root'));
 
 // Before
 // ReactDOM.render(<App />, document.getElementById('root'));
