3bbee7390efa2946d418ceda3590f4e44723c2a3
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-01 18:43:55 +0530

    some code changes

--- src/Pages/Admin/LoginPage.tsx
+++ src/Pages/Admin/LoginPage.tsx
@@ -6,22 +6,19 @@ import { RootState } from "../../ReduxStore/store";
 
 export interface LoginPageProps {
     setIsAuthenticated: any;
+    isAuthenticated: boolean
 }
-const LoginPage: React.FC<LoginPageProps> = ({ setIsAuthenticated }) => {
+const LoginPage: React.FC<LoginPageProps> = ({ setIsAuthenticated, isAuthenticated }) => {
     const navigate = useNavigate()
     const reduxToken = useSelector((state: RootState) => state.User.token)
-    // useEffect(() => {
-    //     const token = localStorage.getItem('Token');
-    //     if (!token && !reduxToken) {
-    //         setIsAuthenticated(false)
-    //     } else {
-    //         setIsAuthenticated(true)
-    //         navigate('/dashboard')
-    //     }
-    // // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/dashboard')
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return(
-        <Login setIsAuthenticated={setIsAuthenticated} />
+        <Login setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />
     )
 }
 
