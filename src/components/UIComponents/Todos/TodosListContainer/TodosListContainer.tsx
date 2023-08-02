3934022d4d265639bf1b7318ef1a68d404b9489b
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-02 11:28:52 +0530

    added logic for child render

--- src/components/UIComponents/Todos/TodosListContainer/TodosListContainer.tsx
+++ src/components/UIComponents/Todos/TodosListContainer/TodosListContainer.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './TodosListContainer.scss'
 import TodoItem from "../TodoItem/TodoListItem";
+import { useDispatch } from "react-redux";
+import { setLoading } from "../../../../ReduxStore/UISlice";
 
 interface TodoListContainerProps {
     todosArray: {
@@ -13,17 +15,27 @@ interface TodoListContainerProps {
         _id: string;
     }[],
     fetchAllUserData: any;
+    isSubTodoContainer?: boolean;
+    parentTodoId?: string;
+    fetchParentTodo?: any
 }
 
-const TodosListContainer: React.FC<Partial<TodoListContainerProps>> = ({ todosArray, fetchAllUserData }) => {
-    if (!todosArray) {
-        return null
-    }
+const TodosListContainer: React.FC<Partial<TodoListContainerProps>> = ({ todosArray, fetchAllUserData, isSubTodoContainer, parentTodoId = "", fetchParentTodo = () => { } }) => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+
+        dispatch(setLoading(true))
+        if (todosArray && todosArray.length > 0) {
+            dispatch(setLoading(false))
+        }
+    }, [dispatch, todosArray])
     return (
         <div className={`todoListItems_container`}>
             {todosArray && todosArray.map((item, index) => {
                 return (
-                    <TodoItem key={item._id} item={item} fetchAllUserData={fetchAllUserData} />
+                    <TodoItem isSubTodo={isSubTodoContainer} parentTodoId={parentTodoId} key={item._id} item={item} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} />
                 )
             })}
         </div>
