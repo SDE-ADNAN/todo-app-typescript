8470371333a2383daf9d1246e63007cd6cc0a8ca
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-05 21:25:22 +0530

    check added

--- src/components/UIComponents/Modal/Modal.tsx
+++ src/components/UIComponents/Modal/Modal.tsx
@@ -3,33 +3,58 @@ import ReactDOM from 'react-dom';
 import './Modal.scss';
 // import AddIcon from '../AddIcon/AddIcon';
 import CrossIcon from '../CrossIcon/CrossIcon';
+import { includeDarkClass } from '../../../CONFIG';
+import { RootState } from '../../../ReduxStore/store';
+import { useSelector } from 'react-redux';
 
 interface ModalProps {
   isOpen: Boolean;
   onClose: () => void;
   children: ReactNode;
   heading: string;
+  mountOnPortal?: boolean;
 }
 
-const Modal: FC<ModalProps> = ({ isOpen, onClose, children, heading = "MOdal header Modal header" }) => {
+const Modal: FC<ModalProps> = ({ isOpen, onClose, children, heading = "MOdal header Modal header" }, mountOnPortal) => {
+  const darkMode = useSelector((state: RootState) => state.UI.theme.dark)
   if (!isOpen) return null;
 
-  return ReactDOM.createPortal(
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <div className='header_and_cosebtn_container'>
-          <div className='modal_header'>
-            <h3 className=''>{heading}</h3>
+  if (mountOnPortal) {
+    return ReactDOM.createPortal(
+      <div className={includeDarkClass("modal-overlay", darkMode)} onClick={onClose}>
+        <div className={includeDarkClass("modal", darkMode)} onClick={(e) => e.stopPropagation()}>
+          <div className={includeDarkClass('header_and_cosebtn_container', darkMode)}>
+            <div className={includeDarkClass('modal_header', darkMode)}>
+              <h3 className={includeDarkClass('', darkMode)}>{heading}</h3>
+            </div>
+            <div onClick={onClose} className={includeDarkClass('modal_close_btn', darkMode)}>
+              <CrossIcon size={20} tooltipText='Close Modal' onClick={() => { }} />
+            </div>
           </div>
-          <div onClick={onClose} className='modal_close_btn'>
-            <CrossIcon size={20} tooltipText='Close Modal' onClick={() => { }} />
-          </div>
+          <div className={includeDarkClass('modal_main_content_container', darkMode)}>{children}</div>
         </div>
-        <div className='modal_main_content_container'>{children}</div>
+      </div>,
+      document.getElementById('modal-root')!
+    );
+  } else {
+    return (
+      <div className={includeDarkClass("modal-overlay", darkMode)} onClick={onClose}>
+        <div className={includeDarkClass("modal", darkMode)} onClick={(e) => e.stopPropagation()}>
+          <div className={includeDarkClass('header_and_cosebtn_container', darkMode)}>
+            <div className={includeDarkClass('modal_header', darkMode)}>
+              <h3 className={includeDarkClass('', darkMode)}>{heading}</h3>
+            </div>
+            <div onClick={onClose} className={includeDarkClass('modal_close_btn', darkMode)}>
+              <CrossIcon size={20} tooltipText='Close Modal' onClick={() => { }} />
+            </div>
+          </div>
+          <div className={includeDarkClass('modal_main_content_container', darkMode)}>{children}</div>
+        </div>
       </div>
-    </div>,
-    document.getElementById('modal-root')!
-  );
+    );
+  }
+
+
 };
 
 export default Modal;
