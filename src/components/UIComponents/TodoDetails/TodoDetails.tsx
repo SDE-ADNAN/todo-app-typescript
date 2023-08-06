12bd794d1dc66fddc9ff221475d799afdf7f6af7
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-06 11:37:07 +0530

    todo update added

--- src/components/UIComponents/TodoDetails/TodoDetails.tsx
+++ src/components/UIComponents/TodoDetails/TodoDetails.tsx
@@ -8,8 +8,10 @@ import TodosListContainer from "../Todos/TodosListContainer/TodosListContainer";
 import Modal from "../Modal/Modal";
 import AddIcon from "../AddIcon/AddIcon";
 import { useDispatch } from "react-redux";
-import { setLoading } from "../../../ReduxStore/UISlice";
+import { setCurrentPage, setLoading } from "../../../ReduxStore/UISlice";
 import LoaderComponent from "../LoaderComponent/LoaderComponent";
+import CTAIconWrapper from "../../WRAPPERS/CTAIconWrapper/CTAIconWrapper";
+import Editsvg from "../../../medias/Editsvg";
 
 interface TodoItem {
     createdAt: string;
@@ -29,13 +31,15 @@ const TodoDetails: React.FC = () => {
     const [updatedAtdateAndTime, setUpdatedAtDateAndTime] =
         useState<Array<string> | null>(null);
     const [isOpen, setIsOpen] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const [subTodoTitleInput, setSubTodoTitleInput] = useState<string | null>(
         null
     );
     const [subTodoDescInput, setSubTodoDescInput] = useState<string | null>(null);
+    // State for input fields
+    const [titleInput, setTitleInput] = useState("");
+    const [descriptionInput, setDescriptionInput] = useState("");
     const params = useParams();
-    // todos/:parentTodo_id/subTodo/:childTodo_id
-    // console.log(params);
     const dispatch = useDispatch();
     const token = useSelector((state: RootState) => state.User.token);
     const darkMode = useSelector((state: RootState) => state.UI.theme.dark);
@@ -139,127 +143,265 @@ const TodoDetails: React.FC = () => {
     };
 
     useEffect(() => {
+        if (todo && todo.title && todo.description) {
+            setTitleInput(todo.title);
+            setDescriptionInput(todo.description);
+        }
+    }, [todo]);
+
+    const handleUpdateSubmit = async (
+        event: React.FormEvent<HTMLFormElement>
+    ) => {
+        event.preventDefault();
+        dispatch(setLoading(true));
+        if (isEditing) {
+            try {
+                if (token !== null) {
+                    const formData = new FormData();
+
+                    if (params.childTodo_id) {
+                        // Update a childTodo
+                        formData.append("todoId", params.childTodo_id);
+                        // formData.append("title", titleInput);
+                        // formData.append("description", descriptionInput);
+                        formData.append(
+                            "changeObj",
+                            JSON.stringify({
+                                title: titleInput,
+                                description: descriptionInput,
+                            })
+                        );
+
+                        const response = await fetch(getUrl("/admin/putSubTodo"), {
+                            method: "PUT",
+                            body: formData,
+                            headers: {
+                                Authorization: token,
+                            },
+                        });
+
+                        if (!response.ok) {
+                            dispatch(setLoading(false));
+                            setIsEditing(false);
+                            setIsOpen(false);
+                            throw new Error("Request failed");
+                        }
+
+                        // Fetch updated childTodo after successful API call
+                        fetchChildTodo(params.childTodo_id, token);
+                    } else if (params.parentTodo_id) {
+                        // Update a parentTodo
+                        formData.append("todoId", params.parentTodo_id);
+                        // formData.append("title", titleInput);
+                        // formData.append("description", descriptionInput);
+                        formData.append(
+                            "changeObj",
+                            JSON.stringify({
+                                title: titleInput,
+                                description: descriptionInput,
+                            })
+                        );
+
+                        const response = await fetch(getUrl("/admin/putTodo"), {
+                            method: "PUT",
+                            body: formData,
+                            headers: {
+                                Authorization: token,
+                            },
+                        });
+
+                        if (!response.ok) {
+                            dispatch(setLoading(false));
+                            setIsEditing(false);
+                            setIsOpen(false);
+                            throw new Error("Request failed");
+                        }
+
+                        // Fetch updated parentTodo after successful API call
+                        fetchParentTodo(params.parentTodo_id, token);
+                    }
+
+                    dispatch(setLoading(false));
+                    setIsEditing(false);
+                    setIsOpen(false);
+                }
+            } catch (err) {
+                console.error("Error:", err);
+                dispatch(setLoading(false));
+                setIsEditing(false);
+                setIsOpen(false);
+            }
+        }
+    };
+
+    useEffect(() => {
         if (token) {
             if (params.parentTodo_id && !params.childTodo_id) {
                 fetchParentTodo(params.parentTodo_id, token);
             } else if (params.parentTodo_id && params.childTodo_id) {
                 fetchChildTodo(params.childTodo_id, token);
+                dispatch(setCurrentPage("Sub-Todo Details"));
             }
         }
-    }, [params.childTodo_id, params.parentTodo_id, token]);
+    }, [dispatch, params.childTodo_id, params.parentTodo_id, token]);
     if (!todo) {
         return <LoaderComponent />;
     }
     return (
         <div className={includeDarkClass("todo_details_container", darkMode)}>
-            <div className={includeDarkClass("todo_id", darkMode)}>
-                <div>
+            <form style={{ width: "100%" }} onSubmit={handleUpdateSubmit}>
+                <div className={includeDarkClass("todo_id", darkMode)}>
                     {" "}
                     Todo ID:
                     {params.parentTodo_id}
+                    <CTAIconWrapper onClick={() => setIsEditing(!isEditing)}>
+                        <Editsvg />
+                    </CTAIconWrapper>
                 </div>
-            </div>
-            <div className={includeDarkClass("horizontal_line", darkMode)}></div>
-            <div className={includeDarkClass("todo_contents_container", darkMode)}>
-                <div className={includeDarkClass("todo_title", darkMode)}>
-                    <div className={includeDarkClass("header", darkMode)}>Title :</div>
-                    <div className={includeDarkClass("content", darkMode)}>
-                        {todo.title}
-                    </div>
-                    <div className={includeDarkClass("horizontal_line", darkMode)}></div>
-                </div>
-                <div className={includeDarkClass("todo_desc", darkMode)}>
-                    <div className={includeDarkClass("header", darkMode)}>
-                        Description :
-                    </div>
-                    <div className={includeDarkClass("content", darkMode)}>
-                        {todo.description}
-                    </div>
-                    <div className={includeDarkClass("horizontal_line", darkMode)}></div>
-                </div>
-                {createdAtdateAndTime && updatedAtdateAndTime && (
-                    <div
-                        className={includeDarkClass(
-                            "todo_date_and_time_container",
-                            darkMode
-                        )}
-                    >
-                        <div className={includeDarkClass("todo_createdAt", darkMode)}>
-                            <div className={includeDarkClass("header", darkMode)}>
-                                CreatedAt :&nbsp;
-                            </div>
-                            <div className={includeDarkClass("date", darkMode)}>
-                                {createdAtdateAndTime[0]}
-                            </div>
-                            <div className={includeDarkClass("time", darkMode)}>
-                                {createdAtdateAndTime[1]}
-                            </div>
-                        </div>
-                        <div className={includeDarkClass("todo_updatedAt", darkMode)}>
-                            <div className={includeDarkClass("header", darkMode)}>
-                                UpdatedAt :&nbsp;{" "}
-                            </div>
-                            <div className={includeDarkClass("date", darkMode)}>
-                                {updatedAtdateAndTime[0]}
-                            </div>
-                            <div className={includeDarkClass("time", darkMode)}>
-                                {updatedAtdateAndTime[1]}
-                            </div>
-                        </div>
-                    </div>
-                )}
                 <div className={includeDarkClass("horizontal_line", darkMode)}></div>
-                {params.parentTodo_id && !params.childTodo_id ? (
-                    <div className={includeDarkClass("todo_subTodos", darkMode)}>
-                        <div className={includeDarkClass("subTodo_addbtn", darkMode)}>
-                            <div className={includeDarkClass("header", darkMode)}>SubTodos :</div>
-                            <div style={{ margin: "5px 0" }} className="button_wrapper">
-                                <button onClick={() => setIsOpen(!isOpen)}>
-                                    <div className="btn_text">Add Sub-Todo</div>
-                                    <AddIcon size={20} showToolTip={false} />
-                                </button>
-                            </div>
+                <div className={includeDarkClass("todo_contents_container", darkMode)}>
+                    <div className={includeDarkClass("todo_title", darkMode)}>
+                        <div className={includeDarkClass("header", darkMode)}>Title :</div>
+                        <div className={includeDarkClass("content", darkMode)}>
+                            {!isEditing ? (
+                                todo.title
+                            ) : (
+                                <input
+                                    type="text"
+                                    value={titleInput}
+                                    onChange={(e) => setTitleInput(e.target.value)}
+                                ></input>
+                            )}
+                        </div>
+                        <div
+                            className={includeDarkClass("horizontal_line", darkMode)}
+                        ></div>
+                    </div>
+                    <div className={includeDarkClass("todo_desc", darkMode)}>
+                        <div className={includeDarkClass("header", darkMode)}>
+                            Description :
+                        </div>
+                        <div className={includeDarkClass("content", darkMode)}>
+                            {!isEditing ? (
+                                todo.description
+                            ) : (
+                                <textarea
+                                    rows={10}
+                                    cols={50}
+                                    value={descriptionInput}
+                                    onChange={(e) => setDescriptionInput(e.target.value)}
+                                ></textarea>
+                            )}
                         </div>
 
-                        <div className={includeDarkClass("todo_desc_desc", darkMode)}>
-                            <TodosListContainer
-                                isSubTodoContainer
-                                parentTodoId={params.parentTodo_id}
-                                todosArray={todo.todo}
-                                fetchParentTodo={fetchParentTodo}
-                            />
-                        </div>
-                        {/* <div className={includeDarkClass("horizontal_line", darkMode)}></div> */}
+
+                        <div
+                            className={includeDarkClass("horizontal_line", darkMode)}
+                        ></div>
                     </div>
-                ) : (
-                    <></>
-                )}
-            </div>
-            <Modal
-                heading="Add a new Todo"
-                isOpen={isOpen}
-                onClose={() => setIsOpen(!isOpen)}
-            >
-                <div className="addTodo_form_container">
-                    <form onSubmit={handleAddSubTodo}>
-                        <input
-                            required
-                            onChange={(e) => setSubTodoTitleInput(e.target.value)}
-                            type="text"
-                            placeholder="Title for your new subTodo ."
-                        />
-                        <input
-                            required
-                            onChange={(e) => setSubTodoDescInput(e.target.value)}
-                            type="textarea"
-                            placeholder="Description for your new subTodo ."
-                        />
-                        <div>
-                            <button>Submit</button>
+                    {isEditing ? (
+                        <div className={includeDarkClass("btn_grp", darkMode)}>
+                            <button type="submit">Submit</button>
+                            <button
+                                type={undefined}
+                                onClick={() => {
+                                    setIsEditing(false);
+                                    setTitleInput(todo.title);
+                                    setDescriptionInput(todo.description);
+                                }}
+                            >
+                                Cancel
+                            </button>
                         </div>
-                    </form>
+                    ) : (
+                        <></>
+                    )}
+                    {createdAtdateAndTime && updatedAtdateAndTime && (
+                        <div
+                            className={includeDarkClass(
+                                "todo_date_and_time_container",
+                                darkMode
+                            )}
+                        >
+                            <div className={includeDarkClass("todo_createdAt", darkMode)}>
+                                <div className={includeDarkClass("header", darkMode)}>
+                                    CreatedAt
+                                </div>
+                                <div className={includeDarkClass("date", darkMode)}>
+                                    {createdAtdateAndTime[0]}
+                                </div>
+                                <div className={includeDarkClass("time", darkMode)}>
+                                    {createdAtdateAndTime[1]}
+                                </div>
+                            </div>
+                            <div className={includeDarkClass("todo_updatedAt", darkMode)}>
+                                <div className={includeDarkClass("header", darkMode)}>
+                                    UpdatedAt
+                                </div>
+                                <div className={includeDarkClass("date", darkMode)}>
+                                    {updatedAtdateAndTime[0]}
+                                </div>
+                                <div className={includeDarkClass("time", darkMode)}>
+                                    {updatedAtdateAndTime[1]}
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    <div className={includeDarkClass("horizontal_line", darkMode)}></div>
+                    {params.parentTodo_id && !params.childTodo_id ? (
+                        <div className={includeDarkClass("todo_subTodos", darkMode)}>
+                            <div className={includeDarkClass("subTodo_addbtn", darkMode)}>
+                                <div className={includeDarkClass("header", darkMode)}>
+                                    SubTodos :
+                                </div>
+                                <div style={{ margin: "5px 0" }} className="button_wrapper">
+                                    <button type={undefined} onClick={(e) => { e.preventDefault(); setIsOpen(!isOpen); }}>
+                                        <div className="btn_text">Add Sub-Todo</div>
+                                        <AddIcon size={20} showToolTip={false} />
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className={includeDarkClass("todo_desc_desc", darkMode)}>
+                                <TodosListContainer
+                                    isSubTodoContainer
+                                    parentTodoId={params.parentTodo_id}
+                                    todosArray={todo.todo}
+                                    fetchParentTodo={fetchParentTodo}
+                                />
+                            </div>
+                            {/* <div className={includeDarkClass("horizontal_line", darkMode)}></div> */}
+                        </div>
+                    ) : (
+                        <></>
+                    )}
                 </div>
-            </Modal>
+                <Modal
+                    heading="Add a new Todo"
+                    isOpen={isOpen}
+                    onClose={() => setIsOpen(!isOpen)}
+                >
+                    <div className="addTodo_form_container">
+                        <form onSubmit={handleAddSubTodo}>
+                            <input
+                                required
+                                onChange={(e) => setSubTodoTitleInput(e.target.value)}
+                                type="text"
+                                placeholder="Title for your new subTodo ."
+                            />
+                            <input
+                                required
+                                onChange={(e) => setSubTodoDescInput(e.target.value)}
+                                type="textarea"
+                                placeholder="Description for your new subTodo ."
+                            />
+                            <div>
+                                <button>Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </Modal>
+            </form>
         </div>
     );
 };
