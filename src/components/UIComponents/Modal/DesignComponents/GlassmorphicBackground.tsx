c52e5ee829fdb6199a6923756a4e7ea63bad37a3
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-22 10:58:41 +0530

    used redux and darkmode

--- src/components/UIComponents/Modal/DesignComponents/GlassmorphicBackground.tsx
+++ src/components/UIComponents/Modal/DesignComponents/GlassmorphicBackground.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './GlassmorphicBackground.scss';
+import { includeDarkClass } from '../../../../CONFIG';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../ReduxStore/store';
 
 interface GlassmorphicBackgroundProps {
     children: React.ReactNode;
 }
 
 const GlassmorphicBackground: React.FC<GlassmorphicBackgroundProps> = ({ children }) => {
+    const darkMode = useSelector((state: RootState) => state.UI.theme.dark)
     return (
-        <div className="glassmorphic-background">
+        <div className={includeDarkClass("glassmorphic-background", darkMode)}>
             {children}
         </div>
     );
