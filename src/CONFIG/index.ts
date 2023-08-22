5f4d20a43bfc2837084dd8da44cdb6f645bc7ee8
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-22 10:58:56 +0530

    changed the date and time func

--- src/CONFIG/index.ts
+++ src/CONFIG/index.ts
@@ -2,35 +2,42 @@ import { API_URL_LIVE, API_URL_LOCAL, isLive } from "../api";
 
 export const getUrl =(remUrl:string) => `${isLive ? API_URL_LIVE : API_URL_LOCAL}${remUrl}`
 
-export function formatDateAndTime(dateObj: Date): [string, string] {
-    const optionsDate: Intl.DateTimeFormatOptions = {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-    };
-  
-    const optionsTime: Intl.DateTimeFormatOptions = {
-      hour: '2-digit',
-      minute: '2-digit',
-    };
-  
-    const dateFormatter = new Intl.DateTimeFormat('en-US', optionsDate);
-    const timeFormatter = new Intl.DateTimeFormat('en-US', optionsTime);
-  
-    const formattedDate = dateFormatter.format(dateObj);
-    const formattedTime = timeFormatter.format(dateObj);
-  
-    return [formattedDate, formattedTime];
+export function formatDateAndTime(dateObj: Date): [string,string] {
+  const day = dateObj.getDate();
+  const month = dateObj.toLocaleString('default', { month: 'short' });
+  const year = dateObj.getFullYear().toString().slice(-2);
+
+  let daySuffix;
+  if (day === 1 || day === 21 || day === 31) {
+    daySuffix = 'st';
+  } else if (day === 2 || day === 22) {
+    daySuffix = 'nd';
+  } else if (day === 3 || day === 23) {
+    daySuffix = 'rd';
+  } else {
+    daySuffix = 'th';
   }
 
-  export const isDarkModeFromLocalStorage = () => {
-    const localStorageDarkMode = localStorage.getItem('darkMode')
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes();
+  const timeOfDay = hours >= 12 ? 'PM' : 'AM';
+  const formattedHours = (hours % 12) || 12; // Convert to 12-hour format
 
-    if (localStorageDarkMode != null && localStorageDarkMode === 'True') {
-        return true;
-    } else {
-        return false;
-    }
+  const formattedTime = `${formattedHours}:${minutes.toString().padStart(2, '0')} ${timeOfDay}`;
+
+  // return `${day}${daySuffix} ${month} ${year} - ${formattedTime}`;
+  return [`${day}${daySuffix} ${month} ${year}`,`${formattedTime}`]
+
+}
+
+export const isDarkModeFromLocalStorage = () => {
+  const localStorageDarkMode = localStorage.getItem('darkMode')
+
+  if (localStorageDarkMode != null && localStorageDarkMode === 'True') {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 
