89ecfb1528b36e84adc5a3476ea74d73efea3d9e
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-09-07 08:12:37 +0530

    all todos changes

--- src/components/UIComponents/Todos/TodosListContainer/TodosListContainer.tsx
+++ src/components/UIComponents/Todos/TodosListContainer/TodosListContainer.tsx
@@ -19,20 +19,22 @@ interface TodoListContainerProps {
         __v: number;
         _id: string;
     }[],
+    isAllTodosContainer: boolean;
     fetchAllUserData: any;
     isSubTodoContainer?: boolean;
     parentTodoId?: string;
     fetchParentTodo?: any;
     className?: string;
     title?: string;
+    hideParent?: boolean;
 }
 
 // const NoTodosSvg: any = () => <div>dsfdsvsd</div>
 
-export const Container: React.FC<Partial<TodoListContainerProps>> = ({ title = '', className = '', todosArray = [], isSubTodoContainer = false, parentTodoId = '', fetchAllUserData = () => { }, fetchParentTodo = () => { } }) => {
+export const Container: React.FC<Partial<TodoListContainerProps>> = ({hideParent=false, title = '', className = '', todosArray = [], isSubTodoContainer = false, parentTodoId = '', fetchAllUserData = () => { }, fetchParentTodo = () => { } }) => {
     const darkMode = useSelector((state: RootState) => state.UI.theme.dark)
     return (
-        <div className={includeDarkClass(`status_container ${className}`, darkMode)}>
+        <div className={includeDarkClass(`status_container ${ hideParent ? 'hideParent' : ''} ${className}`, darkMode)}>
             <div className={includeDarkClass(`title`, darkMode)}>{title}</div>
             <div className={includeDarkClass(`all_todos`, darkMode)}>
                 {todosArray && todosArray.length === 0 ? 'NoTodosSvg' : todosArray ? todosArray.map((item, index) => {
@@ -47,7 +49,7 @@ export const Container: React.FC<Partial<TodoListContainerProps>> = ({ title = '
     )
 };
 
-const TodosListContainer: React.FC<Partial<TodoListContainerProps>> = ({ todosArray, fetchAllUserData, isSubTodoContainer, parentTodoId = "", fetchParentTodo = () => { } }) => {
+const TodosListContainer: React.FC<Partial<TodoListContainerProps>> = ({ isAllTodosContainer, todosArray, fetchAllUserData, isSubTodoContainer, parentTodoId = "", fetchParentTodo = () => { } }) => {
 
     const dispatch = useDispatch()
 
@@ -63,21 +65,30 @@ const TodosListContainer: React.FC<Partial<TodoListContainerProps>> = ({ todosAr
     }, [dispatch, isSubTodoContainer])
     return (
         <div className={includeDarkClass(`todoListItems_container`, darkMode)}>
-            {isSubTodoContainer && todosArray ?
-                <Container title={'Sub-Todos'} className={'Sub-Todos'} todosArray={todosArray} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> : <>
+            {
+            isAllTodosContainer && todosArray ? 
+            <Container title={'All todos'} hideParent={true} className={'Sub-Todos'} todosArray={todosArray} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> 
+            : isSubTodoContainer && todosArray ?
+                <Container title={'Sub-Todos'} className={'Sub-Todos'} todosArray={todosArray} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> 
+                : <>
                     {User && User.statusFiltered && User.statusFiltered.__filteredTodos ?
-                        <Container title={'Todo'} className={'Todo'} todosArray={User.statusFiltered.__filteredTodos} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> : <></>
+                        <Container title={'Todo'} className={'Todo'} todosArray={User.statusFiltered.__filteredTodos} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> 
+                        : <></>
                     }
                     {User && User.statusFiltered && User.statusFiltered.__filteredInProgress ?
-                        <Container title={'InProgress'} className={'InProgress'} todosArray={User.statusFiltered.__filteredInProgress} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> : <></>
+                        <Container title={'InProgress'} className={'InProgress'} todosArray={User.statusFiltered.__filteredInProgress} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> 
+                        : <></>
                     }
                     {User && User.statusFiltered && User.statusFiltered.__filteredCompleted ?
-                        <Container title={'Completed'} className={'Completed'} todosArray={User.statusFiltered.__filteredCompleted} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> : <></>
+                        <Container title={'Completed'} className={'Completed'} todosArray={User.statusFiltered.__filteredCompleted} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> 
+                        : <></>
                     }
                     {User && User.statusFiltered && User.statusFiltered.__filteredOnHold ?
-                        <Container title={'OnHold'} className={'OnHold'} todosArray={User.statusFiltered.__filteredOnHold} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> : <></>
+                        <Container title={'OnHold'} className={'OnHold'} todosArray={User.statusFiltered.__filteredOnHold} isSubTodoContainer={isSubTodoContainer} parentTodoId={parentTodoId} fetchAllUserData={fetchAllUserData} fetchParentTodo={fetchParentTodo} /> 
+                        : <></>
                     }
                 </>}
+
         </div>
     )
 }
